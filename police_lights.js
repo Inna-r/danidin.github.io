816f4576dd1ecf3b9cc35749f6ad1d0c385e97f7
Inna <inna.romanenko90@gmail.com>
2015-05-03 21:15:46 +0300

    added police_lights.js

---
+++ police_lights.js
@@ -0,0 +1,14 @@
+﻿
+window.setInterval(function () { changeLight() }, 100);
+
+function changeLight() {
+    var leftObject = document.getElementById("left");
+    var rightObject = document.getElementById("right");
+    if (leftObject.style.background.indexOf("red") > -1) {
+        leftObject.style.background = "blue";
+        rightObject.style.background = "red";
+    } else {
+        leftObject.style.background = "red";
+        rightObject.style.background = "blue";
+    }
+}
